0a182975f308cd86c77472fb44835f7325f11bae
Lou Dupont <57115124+lou-dupont@users.noreply.github.com>
2020-01-26 11:46:02 +0100

    Add files via upload
    Save as Excel for Excel users

--- traiter_ehpad_esld_ra.R
+++ traiter_ehpad_esld_ra.R
@@ -1,15 +1,17 @@
 options(stringsAsFactors = F)
 
-library(XML)
 library(curl)
+library(data.table)
 library(plyr)
 library(rjson)
+library(xlsx)
+library(XML)
 
 codesDep = formatC(c(1:95, 97), width = 2, flag = 0)
 
 recupererInfos = function(urlSource, codeDep){
   
-  #Chargement de la page 0 pour récupérer le nombre de pages
+  #Chargement de la page 0 pour recuperer le nombre de pages
   urlDep = sprintf(urlSource, codeDep, 0)
   conDep <- curl(urlDep)
   pageParsee = htmlTreeParse(readLines(conDep), useInternalNodes = TRUE)
@@ -17,7 +19,7 @@ recupererInfos = function(urlSource, codeDep){
   
   nbResultats = as.numeric(xpathSApply(pageParsee, "//h2[@id='cnsa_results-total']/b", xmlValue))
   nbPages = trunc(nbResultats/10)
-  cat(sprintf("Il y a %s hébergements dans le département %s.\n", nbResultats, codeDep))
+  cat(sprintf("Il y a %s hebergements dans le departement %s.\n", nbResultats, codeDep))
   
   if (nbResultats == 0){
     return(NULL)
@@ -34,7 +36,7 @@ recupererInfos = function(urlSource, codeDep){
     nodeInfos = gsub('.*(\\{"results"\\:\\[\\{.*\\}\\]\\}).*', '\\1', xmlValue(nodes[[1]]))
     
     results = fromJSON(nodeInfos)$results
-    results = data.table(t(sapply(results, function(x) unlist(lapply(x, function(x) ifelse(is.null(x),'',x))))))
+    results = data.table(t(sapply(results, function(x) unlist(lapply(x, function(x) ifelse(is.null(x), '', x))))))
     
     infosDep[[p+1]] = results
   } 
@@ -52,7 +54,13 @@ construireTableau = function (url) {
   return(tableau)
 }
 
-#### Hébergements *4, Accueil de jour
+sauverTableau = function (tableau, nom) {
+  write.csv(tableau, paste0(nom, '.csv'), row.names = F, fileEncoding = "UTF-8")
+  write.xlsx(tableau, paste0(nom, '.xlsx'), sheetName = "data", col.names = T, row.names = F, append = F)
+  print(paste(nom, ':', nrow(tableau), 'lignes'))
+}
+
+#### Hebergements *4, Accueil de jour
 urlEHPAD_perm = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-ehpad-en-hebergement-permanent/%s/0?page=%s"
 urlEHPAD_temp = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-ehpad-en-hebergement-temporaire/%s/0?page=%s"
 urlESLD = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-esld/%s/0?page=%s"
@@ -61,33 +69,29 @@ urlAccueilJour = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-accueil-
 urlInfoRepit = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-points-dinformation-et-plateformes-de-repit/%s/0?page=%s"
 urlSoinsDomicile = "https://www.pour-les-personnes-agees.gouv.fr/annuaire-soins-et-services-a-domicile/%s/%s"
 
-# Empilement de tous les types d'établissement
+# Empilement de tous les types d'etablissement
 infosESLD = construireTableau(urlESLD)
 infosHebPermanent = construireTableau(urlEHPAD_perm)
 infosHebTemp = construireTableau(urlEHPAD_temp)
 infosResAutonomie = construireTableau(urlResidenceAutonomie)
 infosTousEtablissements = rbind.fill(infosHebPermanent, infosHebTemp, infosESLD, infosResAutonomie)
 infosTousEtablissements = unique(infosTousEtablissements)
-# Pour replacer les colonnes à un meilleur endroit // à améliorer
+# Pour replacer les colonnes au meilleur endroit // faire mieux
 infosTousEtablissements = infosTousEtablissements[, c(1:5, 38, 39, 6:37, 40:ncol(infosTousEtablissements))]
-write.csv(infosTousEtablissements, 'base_ehpad_esld_ra.csv', row.names = F, fileEncoding = "UTF-8", quote = TRUE)
-print(nrow(infosTousEtablissements))
+sauverTableau(infosTousEtablissements, 'base_ehpad_esld_ra')
 
 #### Accueil de jour
 infosAccueilJour = construireTableau(urlAccueilJour)
 infosAccueilJour$dateMaj = NULL
 infosAccueilJour = infosAccueilJour[, !grepl('prixHeb|prixF|tarif|cap_log|pres', colnames(infosAccueilJour), ignore.case = T)]
-write.csv(infosAccueilJour, 'base_accueil_jour.csv', row.names = F, fileEncoding = "UTF-8", quote = TRUE)
-print(nrow(infosAccueilJour))
+sauverTableau(infosAccueilJour, 'base_accueil_jour')
 
 #### Centre CLIC et repit
 infosInfoRepit = construireTableau(urlInfoRepit)
-write.csv(infosInfoRepit, 'base_clic_repit.csv', row.names = F, fileEncoding = "UTF-8", quote = TRUE)
-print(nrow(infosInfoRepit))
+sauverTableau(infosInfoRepit, 'base_clic_repit')
 
-#### Soins à domicile
+#### Soins a domicile
 infosSoinsDomicile = construireTableau(urlSoinsDomicile)
 infosSoinsDomicile$is_esa = NULL
 infosSoinsDomicile$is_handi_vieil = NULL
-write.csv(infosSoinsDomicile, 'base_soins_domicile.csv', row.names = F, fileEncoding = "UTF-8", quote = TRUE)
-print(nrow(infosSoinsDomicile))
+sauverTableau(infosSoinsDomicile, 'base_soins_domicile')
